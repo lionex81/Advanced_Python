718f253ca96815ef4ec843a14cb1bed2c312a721
Oleksandr Kozlov <lionex81@gmail.com>
2022-05-26 22:38:18 +0300

    HW2_OOP

---
+++ HW2_OOP/task_3.py
@@ -0,0 +1,44 @@
+'''3.
+class Profile:
+    """
+    Create regular class taking 8 params on init - name, last_name, phone_number, address, email, birthday, age, sex
+    Override a printable string representation of Profile class and return: list of the params mentioned above
+    """
+'''
+class DictMixin:
+    def to_dict(self):
+        return self._traverse_dict(self.__dict__)
+
+    def _traverse_dict(self, attributes):
+        result = {}
+        for key, value in attributes.items():
+            result[key] = self._traverse(key, value)
+
+        return result
+
+    def _traverse(self, key, value):
+        if isinstance(value, DictMixin):
+            return value.to_dict()
+        elif isinstance(value, dict):
+            return self._traverse_dict(value)
+        elif isinstance(value, list):
+            return [self._traverse(key, v) for v in value]
+        elif hasattr(value, '__dict__'):
+            return self._traverse_dict(value.__dict__)
+        else:
+            return value
+
+
+class Profile(DictMixin):
+    def __init__(self, name, last_name, phone_number, address, email, birthday, age, sex):
+        self.name = name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.address = address
+        self.email = email
+        self.birthday = birthday
+        self.age = age
+        self.sex = sex
+
+p = Profile('Andrew','Kobzar', '+380994567890', 'Kyiv', 'akobzar@ukr.net', '02.12.2001', 21, 'male')
+print(p.to_dict())
